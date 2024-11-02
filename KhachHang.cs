e5a57a6c7778e8e8e7f697722f78b3b0a7ff2545
Nguyenthlinh <162722526+Nguyenthlinh@users.noreply.github.com>
2024-11-02 17:11:23 +0700

    add Koi-Care-System-at-Home

---
+++ KhachHang.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class KhachHangVM
+    {
+        public string Name  { get; set; }
+        public string PassWord {  get; set; }
+    }
+    public class KhachHang : KhachHangVM
+    {
+        public Guid MaKH { get; set; }
+        public string Name { get; set; }
+        public string PassWord { get; set; }
+
+    }
+}
