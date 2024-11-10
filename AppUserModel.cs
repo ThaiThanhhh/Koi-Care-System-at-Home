52953c0e74cbc279b988ad53a602b93bbcbe838e
ThaiThanhhh <hsgegwyahgdhd@gmail.com>
2024-11-10 11:12:36 +0700

    Add files via upload

---
+++ AppUserModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace KoiCareSystem.Models
+
+{
+    public class AppUserModel : IdentityUser
+    {
+        public string Occupation { get; set; }
+
+
+    }
+}
