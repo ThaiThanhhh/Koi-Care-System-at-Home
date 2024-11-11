749488fdd87d194b1600929b734ac85f7df3a3e2
Quyet3526 <quyetcx3526@ut.edu.vn>
2024-11-11 10:38:41 +0700

    Add files via upload

---
+++ ThemCa.cs
@@ -0,0 +1,55 @@
+﻿namespace KoiCareSystem
+{
+    public class KoiCareSystem
+    {
+        public bool ThemKoi(Koi koi)
+        {
+            if (string.IsNullOrEmpty(koi.Name))
+            {
+                return false; // Thất bại nếu tên trống
+            }
+            return true; // Thành công nếu tên hợp lệ
+        }
+    }
+
+    public class Koi
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public string Breed { get; set; }
+    }
+}
+
+namespace KoiCareSystem.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class KoiCareSystemTests
+    {
+        private KoiCareSystem _koiCareSystem;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _koiCareSystem = new KoiCareSystem(); // Tạo đối tượng KoiCareSystem để kiểm thử
+        }
+
+        [TestMethod]
+        public void TestThemCa_ThanhCong()
+        {
+            var koi = new Koi { Name = "Koi D", Age = 1, Weight = 2.0, Breed = "Asagi" };
+            var result = _koiCareSystem.ThemKoi(koi);
+            Assert.IsTrue(result, "Thêm cá Koi thành công.");
+        }
+
+        [TestMethod]
+        public void TestThemCa_ThatBai_KhongCoTen()
+        {
+            var koi = new Koi { Name = "", Age = 1, Weight = 2.0, Breed = "Asagi" };
+            var result = _koiCareSystem.ThemKoi(koi);
+            Assert.IsFalse(result, "Thêm cá Koi thất bại do không có tên.");
+        }
+    }
+}
